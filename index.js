2fdfb098ba1a691ec7d27875d8c477a0996d1f05
bhuvaneswarsankranthi <111528477+Bhuvaneswar26@users.noreply.github.com>
2023-11-13 17:50:18 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,120 @@
+//function to validate the email
+const validate = (email) => {
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("Please enter the right format email");
+        email.reportValidity();
+        return false; 
+    } else {
+        email.setCustomValidity("");
+    }
+}
+
+//function to validate the age
+const validate_age = (dobInput) => {
+    const date = new Date();
+    const y = date.getFullYear();
+    const m = date.getMonth() + 1;
+    const d = date.getDate();
+    const givendob = dobInput.value.split("-");
+    const given_y = Number(givendob[0]);
+    const given_m = Number(givendob[1]);
+    const given_d = Number(givendob[2]);
+    let age = y - given_y;
+    if (m < given_m || (m === given_m && d < given_d)) {
+        age-=1
+    }
+    if (age < 18 || age > 55) {
+        dobInput.setCustomValidity("please make sure age is between 18 and 55");
+        dobInput.reportValidity();
+    } else {
+        dobInput.setCustomValidity(""); 
+    }
+}
+
+let user_details = [];
+
+//function to handle submit
+const submitform = (event) =>{
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const dob = document.getElementById('dob').value;
+    const termsandconditions = document.getElementById('acceptedterms').checked;
+    const pswd = document.getElementById('password').value;
+    const user ={
+        uname :name,
+        uemail:email,
+        udob:dob,
+        utermsandconsitions: termsandconditions ? "Accepted" :"Not accepted",
+        upswd:pswd
+    }
+    user_details.push(user);
+    localStorage.setItem("user_details",JSON.stringify(user_details));
+
+    user_details = get_userdetails();
+    fill_table(user_details);
+}
+
+
+const fill_table = (details)=> {
+    const user_table = document.getElementById('user-table');
+    const user_table_html = details.map((user,index)=>{
+        return (`<tr class="border-2 border-blue-200">
+        <td class="py-2 px-4 bg-blue-200">${user.uname}</td>
+        <td class="py-2 px-4 bg-white-200">${user.uemail}</td>
+        <td class="py-2 px-4 bg-blue-200">${user.upswd}</td>
+        <td class="py-2 px-4 bg-white-200">${user.udob}</td>
+        <td class="py-2 px-6 bg-blue-200">${user.utermsandconsitions}</td>
+        </tr>`)
+    }).join("")
+    user_table.innerHTML =
+
+    `<thead>
+    <tr>
+        <th class="py-2 px-4 bg-blue-600">Name</th>
+        <th class="py-2 px-4 bg-blue-600">Email</th>
+        <th class="py-2 px-4 bg-blue-600">Password</th>
+        <th class="py-2 px-4 bg-blue-600">Dob</th>
+        <th class="py-2 px-4 bg-blue-600">Accepted terms?</th>
+    </tr>
+    </thead>
+    <tbody class="border-2 border-blue-100">
+    ${user_table_html} 
+    </tbody>`
+}
+
+const get_userdetails = ()=>{
+
+    let local_storage_details = localStorage.getItem("user_details");
+
+    if (local_storage_details){
+        
+    local_storage_details = JSON.parse(local_storage_details);
+    }
+    else{
+        local_storage_details = []
+    }
+    return local_storage_details
+}
+
+user_details = get_userdetails();
+
+
+fill_table(user_details);
+
+
+const email = document.getElementById("email");
+
+const dob = document.getElementById("dob");
+
+dob.addEventListener('input',()=>validate_age(dob))
+
+email.addEventListener('input', () => validate(email));
+
+const submit = document.getElementById("reg-form");
+
+submit.addEventListener('submit',submitform);
+
+
+
+
